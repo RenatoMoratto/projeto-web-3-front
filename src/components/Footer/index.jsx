b34fbb8bea7ff2e0e69055545220f779298081b1
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-26 05:33:54 -0300

    style: implement css modules at footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,12 @@
+import styles from "./footer.module.css";
+
 function Footer() {
 	return (
 		<footer>
 			<a href="https://github.com/vercel/next.js/blob/canary/license.md" target="_blank" rel="noreferrer">
 				License: MIT
 			</a>
-			<a href="https://github.com/RenatoMoratto" target="_blank" rel="noreferrer" className="link">
+			<a href="https://github.com/RenatoMoratto" target="_blank" rel="noreferrer" className={styles.link}>
 				GitHub
 			</a>
 		</footer>
