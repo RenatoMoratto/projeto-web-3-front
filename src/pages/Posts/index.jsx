fa19677fddc1e95870a96145372708e11e6b9481
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-26 07:11:14 -0300

    feat: create posts page

---
+++ src/pages/Posts/index.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import Post from "../../components/Post";
+import SearchBar from "../../components/SearchBar";
+
+import styles from "./posts.module.css";
+
+function Posts() {
+	const [posts, setPosts] = useState([]);
+	const [param, setParam] = useState("");
+	const [search, setSearch] = useState(true);
+
+	const searchHandler = () => setSearch(prevState => !prevState);
+	const searchRandomHandler = () => {
+		setParam("/random");
+		setSearch(prevState => !prevState);
+	};
+
+	useEffect(() => {
+		async function getPosts() {
+			try {
+				const response = await fetch(`${process.env.REACT_APP_API_KEY}/posts${param}`);
+				const data = await response.json();
+
+				if (data.length > 0) {
+					setPosts(data);
+				}
+			} catch (error) {
+				alert(error.message);
+			}
+		}
+
+		getPosts();
+	}, [search, param]);
+
+	return (
+		<>
+			<SearchBar
+				onSearch={searchHandler}
+				searchButtons={
+					<button className="btn" onClick={searchRandomHandler}>
+						Random post
+					</button>
+				}
+			/>
+			<main className={styles.posts}>
+				{posts.map((post, i) => (
+					<Post
+						key={post._id}
+						title={post.title}
+						text={post.text}
+						file={"https://source.unsplash.com/600x400/?computer"}
+						user={post.user}
+					/>
+				))}
+			</main>
+		</>
+	);
+}
+
+export default Posts;
