5b532ca485784d03ca15d66f8fc6b4ea6e64f6fc
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-26 05:37:44 -0300

    style: implement css modules at notification

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -1,9 +1,11 @@
+import styles from "./notification.module.css";
+
 function Notification() {
 	return (
-		<section className="notification">
+		<section className={styles.notification}>
 			<a href="#next-12.1">
-				<span className="highlight">New</span>
-				<span className="br"></span>
+				<span className={styles.highlight}>New</span>
+				<span className={styles.br}></span>
 				<b>Next.js 12.1</b>: Including on-demand ISR, support for styled-components and Relay, and our first
 				developer survey →
 			</a>
