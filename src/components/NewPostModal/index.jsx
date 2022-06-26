03e594a402d2186964bcf5588c94130cad0ad7b5
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-26 13:14:36 -0300

    feat: new post modal

---
+++ src/components/NewPostModal/index.jsx
@@ -0,0 +1,99 @@
+import { useContext, useState } from "react";
+import AuthContext from "../../contexts/auth";
+import Modal from "../Modal";
+import Input from "../Input";
+import styles from "./new-post.module.css";
+
+function LoginModal({ onClose, onSearch }) {
+	const { token, user } = useContext(AuthContext);
+	const [title, setTitle] = useState("");
+	const [text, setText] = useState("");
+	const [file, setFile] = useState("");
+	const [fileUrl, setFileUrl] = useState("");
+	const [validation, setValidation] = useState([]);
+
+	const handleFileChange = e => {
+		const fileList = e.target.files;
+		if (!fileList) {
+			return;
+		} else {
+			setFile(fileList[0]);
+			setFileUrl(URL.createObjectURL(fileList[0]));
+		}
+	};
+
+	const submitHandler = async e => {
+		e.preventDefault();
+
+		setValidation([]);
+		const invalidFields = [];
+
+		if (!title || title.length < 3) {
+			invalidFields.push("name");
+		}
+		if (!text || text.length < 3) {
+			invalidFields.push("text");
+		}
+		setValidation(invalidFields);
+
+		if (invalidFields.length === 0) {
+			const formdata = new FormData();
+			formdata.append("title", title);
+			formdata.append("text", text);
+			formdata.append("file", file, file.name);
+			formdata.append("user", user.id);
+
+			try {
+				const response = await fetch(`${process.env.REACT_APP_API_KEY}/post`, {
+					method: "POST",
+					headers: new Headers({ Authorization: `Bearer ${token}` }),
+					body: formdata,
+				});
+
+				if (response.statusCode >= 200 && response.statusCode < 300) {
+					alert("Post created successfully");
+					onSearch();
+					onClose();
+				}
+			} catch (error) {
+				alert(error.message);
+			}
+		}
+	};
+
+	return (
+		<Modal title="New Post" onClose={onClose}>
+			<form id="form" onSubmit={submitHandler}>
+				<Input
+					value={title}
+					setValue={setTitle}
+					id="title"
+					label="Title"
+					errorMessage="Insert a valid title"
+					autoComplete="title"
+					type="title"
+					isInvalid={validation.includes("title")}
+				/>
+				<Input
+					value={text}
+					setValue={setText}
+					id="text"
+					label="Content"
+					errorMessage="Insert some content"
+					type="text"
+					isInvalid={validation.includes("text")}
+				/>
+				{fileUrl !== "" && <img src={fileUrl} width="200rem" alt="" />}
+				<div className={styles["input-group"]}>
+					<label htmlFor={text}>Image</label>
+					<input onChange={handleFileChange} id={text} type="file" />
+				</div>
+				<button className="btn" type="submit">
+					Save post
+				</button>
+			</form>
+		</Modal>
+	);
+}
+
+export default LoginModal;
--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 
 const AuthContext = createContext({
 	token: "",
+	user: { name: "", email: "", id: "" },
 	isLoggedIn: false,
 	login: ({ email, password }) => {},
 	register: ({ username, email, password }) => {},
@@ -9,54 +10,87 @@ const AuthContext = createContext({
 });
 
 export const AuthProvider = ({ children }) => {
-	const tokenInicial = localStorage.getItem("token");
+	const initialToken = localStorage.getItem("token");
+	const initialUser = localStorage.getItem("user");
 
-	const [token, setToken] = useState(tokenInicial);
+	const [token, setToken] = useState(initialToken);
+	const [user, setUser] = useState(JSON.parse(initialUser));
 
 	const isLoggedIn = !!token;
 
 	const LogoutHandler = () => {
 		setToken("");
+		setUser({});
 		localStorage.removeItem("token");
+		localStorage.removeItem("user");
+	};
+
+	const getUser = async (email, token) => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_API_KEY}/user?email=${email}`, {
+				headers: new Headers({ Authorization: `Bearer ${token}` }),
+			});
+
+			const data = await response.json();
+
+			if (response.status >= 200 && response.status < 300) {
+				localStorage.setItem("user", JSON.stringify(data));
+				setUser(data);
+				return;
+			}
+			alert(data.message);
+		} catch (error) {
+			alert(error);
+		}
 	};
 
 	const LoginHandler = async ({ email, password }) => {
-		const response = await fetch(`${process.env.REACT_APP_API_KEY}/login`, {
-			method: "POST",
-			headers: new Headers({ "Content-Type": "application/json" }),
-			body: JSON.stringify({ email, password }),
-		});
+		try {
+			const response = await fetch(`${process.env.REACT_APP_API_KEY}/login`, {
+				method: "POST",
+				headers: new Headers({ "Content-Type": "application/json" }),
+				body: JSON.stringify({ email, password }),
+			});
 
-		const data = await response.json();
+			const data = await response.json();
 
-		if (response.status >= 200 && response.status < 300) {
-			localStorage.setItem("token", JSON.stringify(data.access_token));
-			setToken(data.access_token);
-			return;
+			if (response.status >= 200 && response.status < 300) {
+				getUser(email, data.access_token);
+				localStorage.setItem("token", JSON.stringify(data.access_token));
+				setToken(data.access_token);
+				return;
+			}
+
+			alert(data.message);
+		} catch (error) {
+			alert(error);
 		}
-		
-		alert(data.message);
 	};
 
 	const RegisterHandler = async ({ name, email, password }) => {
-		const response = await fetch(`${process.env.REACT_APP_API_KEY}/register`, {
-			method: "POST",
-			headers: new Headers({ "Content-Type": "application/json" }),
-			body: JSON.stringify({ name, email, password }),
-		});
+		try {
+			const response = await fetch(`${process.env.REACT_APP_API_KEY}/register`, {
+				method: "POST",
+				headers: new Headers({ "Content-Type": "application/json" }),
+				body: JSON.stringify({ name, email, password }),
+			});
 
-		const data = await response.json();
+			const data = await response.json();
 
-		if (response.status === 201) {
-			localStorage.setItem("token", JSON.stringify(data.access_token));
-			setToken(data.access_token);
+			if (response.status === 201) {
+				localStorage.setItem("token", JSON.stringify(data.access_token));
+				setToken(data.access_token);
+			}
+
+			alert(data.message);
+		} catch (error) {
+			alert(error);
 		}
-
-		alert(data.message);
 	};
 
 	const contextValue = {
 		token,
+		user,
 		isLoggedIn: isLoggedIn,
 		login: LoginHandler,
 		register: RegisterHandler,
