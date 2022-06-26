0b4876c0ab56045bdf8f482c4b0ba160c3c45623
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-26 07:17:04 -0300

    style: implement css modules at home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,16 @@
-import Notification from "../components/Notification";
+import Notification from "../../components/Notification";
+import styles from "./home.module.css";
 
 function Home() {
 	return (
 		<>
 			<Notification />
 			<main>
-				<h1 className="title">
+				<h1 className={styles.title}>
 					The React Framework <br />
 					for Production
 				</h1>
-				<p className="description">
+				<p className={styles.description}>
 					Next.js gives you the best developer experience with all the features you need for production:
 					hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more.
 					No config needed.
